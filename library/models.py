9aa00f8ed06e39365783ebbb9c6dbae2dc5c1c71
Ehab Gamal <ehab.g.sh@gmail.com>
2017-04-30 15:24:12 +0200

    Refactoring models

--- library/models.py
+++ library/models.py
@@ -1,27 +1,7 @@
 from django.db import models
+from django.db.models import Avg
 from django.contrib.auth.models import User
-
-
-# class User(models.Model):
-#     name = models.CharField(max_length=255)
-#     email = models.EmailField(blank=False)
-#     password = models.CharField(max_length=255)
-#     img = models.ImageField(upload_to='images/users/')
-#
-#     def __str__(self):
-#         return self.name
-
-
-class Book(models.Model):
-    title = models.CharField(max_length=100)
-    summary = models.TextField(null=True)
-    published_at = models.DateField(null=True)
-    img = models.ImageField(upload_to='images/books/')
-    category = models.ForeignKey('Category', on_delete=models.CASCADE, default=1)
-    author = models.ForeignKey('Author', on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.title
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Author(models.Model):
@@ -30,6 +10,7 @@ class Author(models.Model):
     died_at = models.DateField(blank=True)
     bio = models.TextField(null=True)
     img = models.ImageField(upload_to='images/authors/')
+    users = models.ManyToManyField(User, blank=True)
 
     def __str__(self):
         return self.name
@@ -37,28 +18,46 @@ class Author(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
+    desc = models.CharField(max_length=255, default='')
+    users = models.ManyToManyField(User, blank=True)
 
     def __str__(self):
         return self.name
 
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    authors = models.ManyToManyField('Author')
-    categories = models.ManyToManyField('Category')
-    books = models.ManyToManyField('Book', through='Rate')
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    summary = models.TextField(null=True)
+    published_at = models.DateField(null=True)
+    img = models.ImageField(upload_to='images/books/')
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, default=1)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    users = models.ManyToManyField(User, through='UserBooks')
 
     def __str__(self):
-        return self.user.email
+        return self.title
+
+    @property
+    def rate(self):
+        try:
+            return int(self.userbooks_set.filter(rate__gt=0).aggregate(Avg('rate'))['rate__avg'])
+        except:
+            return 0
+
+    @property
+    def rate_count(self):
+        try:
+            return self.userbooks_set.filter(rate__gt=0).count()
+        except:
+            return 0
 
 
-class Rate(models.Model):
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+class UserBooks(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
-    STATES = [('read', 'Read'), ('wish', 'Wish'), ('none', 'None')]
-    state = models.CharField(max_length=10, choices=STATES, default='none')
-    RATES = [(1, "1"), (2, "2"), (3, "3"), (4, "4"), (5, "5")]
-    rate = models.CharField(max_length=1, choices=RATES)
+    rate = models.IntegerField(default=0, validators=[MaxValueValidator(5), MinValueValidator(0)])
+    wish = models.BooleanField(default=False)
+    read = models.BooleanField(default=False)
 
-    def __str__(self):
-        return self.rate
+    class Meta:
+        unique_together = ('user', 'book')
